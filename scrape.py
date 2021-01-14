04a9d0ec6a3cf92b4e4c073365e765976604eb23
John Reynolds <c18john.reynolds@gmail.com>
2021-01-14 18:43:22 +0000

    Created function that enumerates links and the scrapes just the title, link, and votes from the page and adds to a list. Also import print to clean up output via print statement.

--- scrape.py
+++ scrape.py
@@ -1,5 +1,6 @@
 import requests
 from bs4 import BeautifulSoup
+import pprint
 
 #pull response data from website page using requests.get
 res = requests.get('https://news.ycombinator.com/news')
@@ -7,5 +8,19 @@ res = requests.get('https://news.ycombinator.com/news')
 soup = BeautifulSoup(res.text, 'html.parser')
 #using css selectors, store list of links and votes
 links = soup.select('.storylink')
-votes = soup.select('.score')
-votes.get()
+subtext = soup.select('.subtext')
+
+#create list of link titles from links provided with votes over 100.
+def create_custom_hn(links, subtext):
+  hn = []
+  for index, item in enumerate(links):
+    title = item.getText()
+    href = item.get('href', None)
+    vote = subtext[index].select('.score')
+    if len(vote):
+      points = int(vote[0].getText().replace(' points', ''))
+      hn.append({'title': title, 'link': href, 'votes': points})
+
+  return hn
+
+pprint.pprint(create_custom_hn(links, subtext))
